867e53acce0943f6c2b6f99273828c2628cd38ec
Anish PJ <ljcanish@gmail.com>
2022-12-07 19:16:14 +0530

    Sample
    Sample

--- Sample.js
+++ Sample.js
@@ -1,1 +1,30 @@
 
+
+// for Loop
+let arr = [1,2,3,4,5,6,7]
+for (let i = 0; i < arr.length; i++) {
+    
+    
+}
+
+// for each
+array.forEach(element => {
+    
+});
+
+array.forEach(element => {
+    
+});
+
+//for in
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+
+//for of
+for (const iterator of object) {
+    
+}
