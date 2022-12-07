7f3f963e7a663919300c018005f370223c678057
Anish PJ <ljcanish@gmail.com>
2022-12-08 01:29:41 +0530

    sample

--- Sample.js
+++ Sample.js
@@ -1,30 +1,40 @@
+var StudentMark = [
 
+    {name: "Student 1", mark: {tamil: 60 , English: 95}},
+    {name: "Student 2", mark: {tamil: 50 , English: 100}}
+    
+    ]
+ 
 
 // for Loop
-let arr = [1,2,3,4,5,6,7]
-for (let i = 0; i < arr.length; i++) {
-    
-    
+let totalAllMarks = 0
+for (let i = 0; i < StudentMark.length; i++) {
+    var totalMarks = StudentMark[i].mark.English + StudentMark[i].mark.tamil
+    console.log(StudentMark[i].name +":"+totalMarks);
+    totalAllMarks += totalMarks;
 }
+console.log("Total marks of Student 1 & Student 2 : "+totalAllMarks);
 
 // for each
-array.forEach(element => {
-    
-});
+// let totalAllMarks1 = [];
+// let s1 = StudentMark.forEach( mark => {
+//    var stu = StudentMark.mark;
+//      totalAllMarks1.push.mark
+//     return  
+// });
+// console.log(s1);
+// console.log(totalAllMarks1);
 
-array.forEach(element => {
-    
-});
 
-//for in
-for (const key in object) {
-    if (Object.hasOwnProperty.call(object, key)) {
-        const element = object[key];
+// //for in
+// for (const key in object) {
+//     if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
         
-    }
-}
+//     }
+// }
 
-//for of
-for (const iterator of object) {
+// //for of
+// for (const iterator of object) {
     
-}
+// }
