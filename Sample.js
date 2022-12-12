7999f29347392d18b3ecf3c547ad6c6687919caf
Anish PJ <ljcanish@gmail.com>
2022-12-12 17:55:36 +0530

    12Dec2022

--- Sample.js
+++ Sample.js
@@ -1,52 +1,24 @@
 var StudentMark = [
-
     {name: "Student 1", mark: {tamil: 60 , English: 95}},
     {name: "Student 2", mark: {tamil: 50 , English: 100}}
-    
     ]
- 
 
-// for Loop
 let totalAllMarks = 0
 for (let i = 0; i < StudentMark.length; i++) {
     var totalMarks = StudentMark[i].mark.English + StudentMark[i].mark.tamil
     console.log(StudentMark[i].name +":"+totalMarks);
     totalAllMarks += totalMarks;
 }
-console.log("Total marks of Student 1 & Student 2 : "+totalAllMarks);
-
-// for each
-// let totalAllMarks1 = [];
-// let s1 = StudentMark.forEach( mark => {
-//    var stu = StudentMark.mark;
-//      totalAllMarks1.push.mark
-//     return  
-// });
-// console.log(s1);
-// console.log(totalAllMarks1);
-
-
-// //for in
-// for (const key in object) {
-//     if (Object.hasOwnProperty.call(object, key)) {
-//         const element = object[key];
-        
-//     }
-// }
-
-// //for of
-// for (const iterator of object) {
-    
-// }
+console.log("Total marks of Student 1 & Student 2 : "+totalAllMarks)
 
 
 
+let name1 = "";
+let Mark = "";
+for (let i in StudentMark) {
+        name1 = StudentMark[i].name;
+        Mark = StudentMark[i].mark;
 
-
-
-
-
-
-
-
-
+    console.log(name1 );
+    console.log( Mark );
+}
